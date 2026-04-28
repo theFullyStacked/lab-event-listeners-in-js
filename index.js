51362cd8ee6a1727dc5fc78400b2b5f573b52e7f
William Munene <williammunene2018@gmail.com>
2026-04-28 13:40:52 +0300

    working build

--- index.js
+++ index.js
@@ -2,29 +2,36 @@
 
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
-  // Implement the function to change background color
+  const colors = ['rgb(222, 12, 12)','rgb(12, 222, 33)','rgb(12,66,222)'];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = randomColor;
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
-  // Implement the function to reset background color
+  document.body.style.backgroundColor='';
 }
 
 // Capture Keyboard Input
 
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
-  // Implement the function to display key pressed
+  const key=document.getElementById('textInput');
+  const preview=document.getElementById('keyPressDisplay');
+  preview.textContent="Key pressed: "+event.key;
 }
 
 // Process Text Input
 
 // Function to display user input in real-time
 function displayUserInput() {
-  // Implement the function to display user input
+  const text=document.getElementById('textInput');
+  const preview=document.getElementById('textInputDisplay');
+  preview.textContent="You typed: "+text.value;
 }
 
 // Attach Event Listeners
+
 function setupEventListeners() {
 // Attach event listener to change background color when the button is clicked
   document
